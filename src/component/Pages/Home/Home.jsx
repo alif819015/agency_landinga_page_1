6a8ff61b772987cf99a021ff48432ab7486924e7
tuamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-08-14 03:04:24 +0600

    experiance & Review Section is added

--- src/component/Pages/Home/Home.jsx
+++ src/component/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "../Banner/Banner";
 import About from "../About/About";
+import Experiance from "../Experiance/Experiance";
+import ReviewSection from "../ReviewSection/ReviewSection";
 
 const Home = () => {
   return (
     <div className="bg-black text-white">
       <Banner/>
       <About/>
+      <Experiance></Experiance>
+      <ReviewSection></ReviewSection>
     </div>
   );
 };
